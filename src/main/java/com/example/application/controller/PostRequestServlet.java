53f1bdb33551ce617ef6112e9b97901f3e6fde05
Giovanni Lovato <giovanni@lova.to>
2021-09-28 13:45:47 +0200

    Add submit form

--- src/main/java/com/example/application/controller/PostRequestServlet.java
+++ src/main/java/com/example/application/controller/PostRequestServlet.java
@@ -3,24 +3,36 @@ package com.example.application.controller;
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vaadin.collaborationengine.UserInfo;
 
 @RestController
+@RequestMapping("/submit")
 public class PostRequestServlet {
+
+    private static final String FORM = "<form method=\"post\">"
+            + "<input name=\"message\"><button type=\"submit\">Submit</button>"
+            + "</form>";
 
     @Autowired
     private ServletContext servletContext;
 
     private final UserInfo userInfo = new UserInfo("ext", "External");
 
-    @PostMapping("/submit")
+    @PostMapping(produces = "text/html")
     public String submitMessage(@RequestParam("message") String message) {
         // Add code here
-        return message;
+        return FORM;
+    }
+
+    @GetMapping(produces = "text/html")
+    public String showForm() {
+        return FORM;
     }
 
 }
