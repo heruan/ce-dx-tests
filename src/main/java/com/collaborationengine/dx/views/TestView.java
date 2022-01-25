782f65bd2565fc71649e7c444d136fc968ded39b
Giovanni Lovato <giovanni@lova.to>
2022-01-25 18:51:50 +0100

    Add CollaborationList API test view

--- src/main/java/com/collaborationengine/dx/views/TestView.java
+++ src/main/java/com/collaborationengine/dx/views/TestView.java
@@ -1,30 +1,49 @@
 package com.collaborationengine.dx.views;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.collaborationengine.CollaborationEngine;
+import com.vaadin.collaborationengine.CollaborationList;
+import com.vaadin.collaborationengine.TopicConnection;
+import com.vaadin.collaborationengine.UserInfo;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.Registration;
 
 @PageTitle("CE DX Test")
 @Route(value = "", layout = MainLayout.class)
 public class TestView extends VerticalLayout {
 
+    final UserInfo localUser = new UserInfo("local");
+
+    final Button addButton = new Button("Add an item to the list");
+
+    final Button showButton = new Button("Show the current item value");
+
+    final Button replaceButton = new Button("Replace the item value");
+
+    final Button removeButton = new Button("Remove the item from the list");
+
     public TestView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        add(addButton, showButton, replaceButton, removeButton);
+        CollaborationEngine.getInstance().openTopicConnection(this, "dx-test",
+                localUser, this::onConnectionActivate);
     }
 
+    Registration onConnectionActivate(TopicConnection connection) {
+        CollaborationList values = connection.getNamedList("values");
+        Registration r1 = addButton.addClickListener(event -> {
+            // Insert an item with the value "foo" in the list
+        });
+        Registration r2 = showButton.addClickListener(event -> {
+            // Show a notification with the current item value
+        });
+        Registration r3 = replaceButton.addClickListener(event -> {
+            // Replace the item value with "bar"
+        });
+        Registration r4 = removeButton.addClickListener(event -> {
+            // Remove the item from the list
+        });
+        return Registration.combine(r1, r2, r3, r4);
+    }
 }
